49ebd44075b4d01793d3dc1e7583c1a46bf466f6
tsseo <tsseo.dev@gmail.com>
2021-12-08 15:03:36 +0900

    Initial commit

---
+++ my-BulletinBoard/src/main/java/net/bulletinboard/MyBulletinBoardApplication.java
@@ -0,0 +1,13 @@
+package net.bulletinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBulletinBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBulletinBoardApplication.class, args);
+    }
+
+}
---
+++ my-BulletinBoard/src/test/java/net/bulletinboard/MyBulletinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bulletinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBulletinBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

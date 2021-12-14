8a36f83a1848fa02d77866773bef63fde1c44ca6
tsseo <tsseo.dev@gmail.com>
2021-12-14 13:13:18 +0900

    초기화면 Controller 추가 updateForom 추가

--- my-BulletinBoard/src/main/java/net/bulletinboard/web/UserController.java
+++ my-BulletinBoard/src/main/java/net/bulletinboard/web/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,6 +23,18 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping("/form")
+    public String form() {
+        return "/user/form";
+    }
+
+    @GetMapping("/{id}/form")
+    public String updateForm(@PathVariable Long id, Model model) {
+//        model.addAttribute("user", userRepository.findOne(id));
+        model.addAttribute("user", userRepository.findById(id).get());
+        return "/user/updateForm";
+    }
 
     @PostMapping("")
     public String create(User user) {

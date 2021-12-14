225855cbd4dbf70e2133c560e57934dc011470e5
tsseo <tsseo.dev@gmail.com>
2021-12-14 14:14:38 +0900

    개인정보 update 추가 put method 사용을 위해 spring.mvc.hiddenmethod.filter.enabled=true 설정 추가

--- my-BulletinBoard/src/main/java/net/bulletinboard/web/UserController.java
+++ my-BulletinBoard/src/main/java/net/bulletinboard/web/UserController.java
@@ -5,10 +5,7 @@ import net.bulletinboard.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 /**
@@ -31,12 +28,19 @@ public class UserController {
 
     @GetMapping("/{id}/form")
     public String updateForm(@PathVariable Long id, Model model) {
-//        model.addAttribute("user", userRepository.findOne(id));
         model.addAttribute("user", userRepository.findById(id).get());
         return "/user/updateForm";
     }
 
-    @PostMapping("")
+    @PutMapping("/{id}")
+    public String update(@PathVariable Long id, User newUser) {
+        User user = userRepository.findById(id).get();
+        user.update(newUser);
+        userRepository.save(user);
+        return "redirect:/users";
+    }
+
+     @PostMapping("")
     public String create(User user) {
         System.out.println("user = " + user);
         userRepository.save(user);

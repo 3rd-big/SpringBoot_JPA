8a504f513aaa135d73d739ac6398c602ed495ae2
tsseo <tsseo.dev@gmail.com>
2021-12-22 16:07:53 +0900

    Fix 질문하기 기능 완료

--- my-BulletinBoard/src/main/java/net/bulletinboard/web/QuestionController.java
+++ my-BulletinBoard/src/main/java/net/bulletinboard/web/QuestionController.java
@@ -5,7 +5,9 @@ import net.bulletinboard.domain.QuestionRepository;
 import net.bulletinboard.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,9 +18,8 @@ import javax.servlet.http.HttpSession;
 public class QuestionController {
 
     /**
-     *  @Autowired
-     *  QuestionRepository 는 interface, 스프링이 구현체를 만들어 관리함
-     *  우리는 해당 애너테이션을 사용해서 해당 구현체를 가져다씀
+     * @Autowired QuestionRepository 는 interface, 스프링이 구현체를 만들어 관리함
+     * 우리는 해당 애너테이션을 사용해서 해당 구현체를 가져다씀
      */
     @Autowired
     private QuestionRepository questionRepository;
@@ -44,5 +45,11 @@ public class QuestionController {
         Question newQuestion = new Question(sessionUser, title, contents);
         questionRepository.save(newQuestion);
         return "redirect:/";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        model.addAttribute("question", questionRepository.findById(id).get());
+        return "/qna/show";
     }
 }
